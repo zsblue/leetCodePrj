86f804167be13fe87fe3fb1ddb086f162e3cfa9f
张松 <zhangsong@shanlitech.com>
2021-07-16 16:05:58 +0800

    1

--- src/main/java/com/leet/code/Leet0350.java
+++ src/main/java/com/leet/code/Leet0350.java
@@ -1,10 +1,7 @@
 package com.leet.code;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import javax.swing.text.html.parser.Entity;
+import java.util.*;
 
 /**
  * **
@@ -23,9 +20,9 @@ import java.util.Set;
  * .*                   @        ***
  * **
  *
- * @author by zhangsong 2021/7/15
+ * @author by zhangsong 2021/7/16
  * <p>
- * 349. 两个数组的交集
+ * 350. 两个数组的交集 II
  * 给定两个数组，编写一个函数来计算它们的交集。
  * <p>
  * <p>
@@ -33,16 +30,27 @@ import java.util.Set;
  * 示例 1：
  * <p>
  * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
- * 输出：[2]
- * 示例 2：
+ * 输出：[2,2]
+ * 示例 2:
  * <p>
  * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
- * 输出：[9,4]
+ * 输出：[4,9]
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现次数的最小值一致。
+ * 我们可以不考虑输出结果的顺序。
+ * <p>
+ * 进阶：
+ * <p>
+ * 如果给定的数组已经排好序呢？你将如何优化你的算法？
+ * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+ * 如果 nums2 的元素存储在磁盘上，内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
  */
-public class Leet0349 {
-
+public class Leet0350 {
     public static void main(String[] args) {
-        Leet0349 leet = new Leet0349();
+        Leet0350 leet = new Leet0350();
 
         int[] nums1, nums2;
         int[] result, verify;
@@ -50,14 +58,14 @@ public class Leet0349 {
         nums1 = new int[]{1, 2, 2, 1};
         nums2 = new int[]{2, 2};
         verify = new int[]{2};
-        result = leet.intersection(nums1, nums2);
+        result = leet.intersect(nums1, nums2);
         check(result, verify);
 
 
         nums1 = new int[]{4, 9, 5};
         nums2 = new int[]{9, 4, 9, 8, 4};
         verify = new int[]{9, 4};
-        result = leet.intersection(nums1, nums2);
+        result = leet.intersect(nums1, nums2);
         check(result, verify);
 
     }
@@ -86,85 +94,38 @@ public class Leet0349 {
 
     }
 
-    public int[] intersection2(int[] nums1, int[] nums2) {
+    public int[] intersect2(int[] nums1, int[] nums2) {
 
-
-        Arrays.sort(nums1);
-        Set<Integer> set = new LinkedHashSet<>();
-
-
-        for (int i = 0; i < nums2.length; i++) {
-            if (i > 0 && nums2[i] == nums2[i - 1]) {
-                continue;
-            }
-            boolean exist = find(nums1, nums2[i]);
-
-            if (exist) {
-                set.add(nums2[i]);
-            }
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
         }
-
-        int[] result = new int[set.size()];
-
-        int i = 0;
-        for (Integer item : set) {
-
-            result[i++] = item;
-
-        }
-        return result;
-
-    }
-
-    private boolean find(int[] nums, int target) {
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = (right - left) / 2 + left;
-            if (nums[mid] == target) {
-                return true;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        return false;
-    }
-
-
-    public int[] intersection3(int[] nums1, int[] nums2) {
-
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
+        Map<Integer, Integer> hash1 = new HashMap<>();
 
         for (int i = 0; i < nums1.length; i++) {
-            set1.add(nums1[i]);
+            int count = hash1.getOrDefault(nums1[i], 0) + 1;
+            hash1.put(nums1[i], count);
         }
-        for (int i = 0; i < nums2.length; i++) {
-
-            if (set1.contains(nums2[i])) {
-                set2.add(nums2[i]);
+        int index = 0;
+        int[] result = new int[nums1.length];
+        for (int num : nums2) {
+            int count = hash1.getOrDefault(num, 0);
+            if (count > 0) {
+                result[index++] = num;
+                count--;
+                if (count > 0) {
+                    hash1.put(num, count);
+                } else {
+                    hash1.remove(num);
+                }
             }
         }
-
-        int[] result = new int[set2.size()];
-
-        int m = 0;
-        for (int item : set2) {
-            result[m++] = item;
-
-        }
-        return result;
+        return Arrays.copyOfRange(result, 0, index);
     }
 
-    public int[] intersection(int[] nums1, int[] nums2) {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-
         int len1 = nums1.length, len2 = nums2.length;
         int[] result = new int[Math.min(len1, len2)];
         int index1 = 0, index2 = 0, index = 0;
@@ -174,13 +135,12 @@ public class Leet0349 {
             } else if (nums1[index1] < nums2[index2]) {
                 index1++;
             } else {
-                if (index == 0 || nums1[index1] != result[index - 1]) {
-                    result[index++] = nums1[index1];
-                }
+                result[index++] = nums1[index1];
                 index1++;
                 index2++;
             }
         }
         return Arrays.copyOfRange(result, 0, index);
     }
+
 }
