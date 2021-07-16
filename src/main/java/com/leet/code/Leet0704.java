18c688364b449a6d338c7f081015576770004d54
张松 <zhangsong@shanlitech.com>
2021-07-16 18:00:59 +0800

    1

---
+++ src/main/java/com/leet/code/Leet0704.java
@@ -0,0 +1,83 @@
+package com.leet.code;
+
+/**
+ * **
+ * .*                   @
+ * .*           @@@@     @@
+ * .*        @@@    @@   @* @             /@/@@/@/@@/@/
+ * .*      @@@       @@  @ * @           /@/  /@/  /@/
+ * .*    @@         @@  @ * @           /@/  /@/  /@/
+ * .*  @@          @@  @ * @           /@/  /@/  /@/
+ * .* @@          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ * .*  @@          @@  @ * @     @ @   \@\  \@\  \@\
+ * .*    @@         @@  @ * @    @ @    \@\  \@\  \@\
+ * .*      @@@       @@  @ * @  @ * @    \@\  \@\  \@\
+ * .*        @@@    @@   @* @  @ * * @    \@\@@\@\@@\@\
+ * .*           @@@@     @@    @@*@*@@
+ * .*                   @        ***
+ * **
+ *
+ * @author by zhangsong 2021/7/16
+ * <p>
+ * 704. 二分查找
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ * 示例 2:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 你可以假设 nums 中的所有元素是不重复的。
+ * n 将在 [1, 10000]之间。
+ * nums 的每个元素都将在 [-9999, 9999]之间。
+ */
+public class Leet0704 {
+
+
+    public static void main(String[] args) {
+        Leet0704 leet = new Leet0704();
+        int[] nums;
+        int target, result, verify;
+
+        nums = new int[]{-1, 0, 3, 5, 9, 12};
+        target = 9;
+        verify = 4;
+        result = leet.search(nums, target);
+        System.out.println("===>" + result + ";" + verify);
+
+        nums = new int[]{-1, 0, 3, 5, 9, 12};
+        target = 2;
+        verify = -1;
+        result = leet.search(nums, target);
+        System.out.println("===>" + result + ";" + verify);
+    }
+
+    public int search(int[] nums, int target) {
+
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+
+    }
+}
